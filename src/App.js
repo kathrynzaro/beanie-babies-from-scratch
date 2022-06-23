c49c90e27074fe5cccf52fb906c98d1ec552547a
kathrynzaro <zarokathryn@gmail.com>
2022-06-23 09:50:59 -0700

    linking supabase and adding files / routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import RestaurantsPage from './RestaurantsPage';
+import RestaurantDetail from './RestaurantDetail';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route exact path="/">
+            <RestaurantsPage />
+            {/* this home page route should list all the beanies */}
+          </Route>
+          <Route exact path="/beanies/:id"> 
+            <RestaurantDetail />
+            {/* this route should point to a particulat beanie baby by id and render that specific BeanieDetail page */}
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/RestaurantsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function RestaurantsPage() {
+  return (
+    <div>RestaurantsPage</div>
+  );
+}
